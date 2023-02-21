a68cfde74155b75e73753dd6bf04a88d3d6a5414
= <=>
2023-02-21 08:15:40 +0700

    add serach features

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,7 +16,9 @@ const RenderCards = ({ data, title }) => {
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
+  const [searchedResults, setSearchedResults] = useState(null);
   const [searchText, setSearchText] = useState('');
+  const [searchTimeout, setSearchTimeout] = useState(null);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -29,11 +31,10 @@ const Home = () => {
             headers: { 'Content-Type': 'application/json' },
           }
         );
-        // console.log(response);
+
         if (response.ok) {
           const result = await response.json();
           setAllPosts(result.data.reverse());
-          console.log(result.data.reverse());
         }
       } catch (error) {
         alert(error);
@@ -44,6 +45,26 @@ const Home = () => {
 
     fetchPosts();
   }, []);
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = allPosts.filter(
+          (item) =>
+            item.name
+              .toLowerCase()
+              .includes(searchText.toLowerCase()) ||
+            item.prompt
+              .toLowerCase()
+              .includes(searchText.toLowerCase())
+        );
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -58,7 +79,14 @@ const Home = () => {
       </div>
 
       <div className="mt-16">
-        <FormField />
+        <FormField
+          labelName={'Search posts'}
+          type="text"
+          name="text"
+          placeholder="Search posts"
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
       </div>
 
       <div className="mt-10">
@@ -78,7 +106,7 @@ const Home = () => {
             <div className="grid lg:gird-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
               {searchText ? (
                 <RenderCards
-                  data={[]}
+                  data={searchedResults}
                   title="No search results found"
                 />
               ) : (
